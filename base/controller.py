9703fd5314a334a8e4adfbd1e91789298be8f2df
Lock <luxstme@gmail.com>
2023-11-26 13:43:26 -0600

    class based views started

--- base/controller.py
+++ base/controller.py
@@ -1,2 +1,22 @@
-def test_1():
+from django.shortcuts import render, redirect
+from django.db.models import Q
+
+from .models import Medication
+
+def get(request):
+    q = request.GET.get('q', None)
+
+    if not q:
+        medications = Medication.objects.all().order_by('name')
+
+    else:
+        medications = Medication.objects.filter(
+            Q(name__icontains = q)
+        ).order_by('name')
+
+    context = {'medications': medications}
+    return render(request, 'base/home.html', context)
+
+def post(self, request):
     pass
+
