d3bf80d3e145f02624bf4987abeb9151f4f55ed8
lhunter50 <luxstme@gmail.com>
2025-04-06 21:49:03 +0100

    added documentation

--- medication-fe/components/NewMedicationForm.js
+++ medication-fe/components/NewMedicationForm.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 import { API_URL } from '../src/constants'
 
 class NewMedicationForm extends React.Component {
-    //set initial state
+    //set initial state for form
     state={
         pk:0,
         name:'',
@@ -17,6 +17,7 @@ class NewMedicationForm extends React.Component {
         frequency:'',
     }
 
+    // will run after this component finishes start up, will set the state to these props IF they exist (used for editing our medication)
     componentDidMount() {
         if (this.props.medication) {
             const { pk, name, classification, implications, dose, route, frequency } = this.props.medication;
@@ -24,10 +25,12 @@ class NewMedicationForm extends React.Component {
         }
     }
 
+    // will handle updating for props value in each field
     onChange = e => {
         this.setState({ [e.target.name] : e.target.value }) 
     };
 
+    // will deal with our POST requests when we submit the form, when we submit it will call this function and trigger the axios post() function
     createMedication = e => {
         e.preventDefault();
         axios.put(API_URL + this.state.pk, this.state).then(() => {
@@ -36,6 +39,7 @@ class NewMedicationForm extends React.Component {
         })
     }
 
+    // also works with our POST calls, instead of creating a new medication we will edit an existing med.
     editMedication = e => {
         e.preventDefault();
         axios.put(API_URL + this.state.pk, this.state).then(() => {
