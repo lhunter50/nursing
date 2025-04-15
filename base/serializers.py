80dffc031f29f20ded131ef298888fee15501dde
lhunter50 <luxstme@gmail.com>
2025-04-15 01:47:23 +0100

    fixed intention serializer

--- base/serializers.py
+++ base/serializers.py
@@ -4,4 +4,4 @@ from .models import Medication
 class MedicationSerializers(serializers.ModelSerializer):
     class Meta:
         model = Medication
-        fields = ['id', 'name', 'classification', 'implications', 'dose', 'route', 'frequency']
+        fields = ['id', 'name', 'intention', 'classification', 'implications', 'dose', 'route', 'frequency']
