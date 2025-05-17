58c0731640e3008b126a5a41f7af99751e7be037
lhunter50 <luxstme@gmail.com>
2025-05-17 19:21:47 +0100

    set up function based views

--- base/serializers.py
+++ base/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Medication
 
-class MedicationSerializers(serializers.ModelSerializer):
+class MedicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Medication
         fields = ['id', 'name', 'intention', 'classification', 'implications', 'dose', 'route', 'frequency']
