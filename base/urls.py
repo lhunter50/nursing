5617601f9a2f7eae69f778bf82f5a4345c206ca9
lhunter50 <luxstme@gmail.com>
2025-05-17 21:58:32 +0100

    added the rest of the views and update urls

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
-from base.views import MedicationList, CreateMedication
+from django.urls import path
+from base.views import MedicationList, CreateMedication, MedicationDetail, MedicationUpdate, MedicationDelete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('medication/',
-         MedicationList.as_view(),
-         name='medication-list'),
-    path('medication/create/', CreateMedication.as_view(), name='create-medication')    
+    path('medication/', MedicationList.as_view(), name='medication-list'),
+    path('medication/create/', CreateMedication.as_view(), name='create-medication'),
+    path('medication/<uuid:medication_id>/', MedicationDetail.as_view(), name='medication-detail'),
+    path('medication/<uuid:medication_id>/update/', MedicationUpdate.as_view(), name='medication-update'),
+    path('medication/<uuid:medication_id/delete', MedicationDelete.as_view(), name='medication-delete'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
 from base.models import Medication
 from base.serializers import MedicationSerializer
@@ -8,8 +8,19 @@ class MedicationList(ListAPIView):
   queryset = Medication.objects.all()
   serializer_class = MedicationSerializer
     
-
 class CreateMedication(CreateAPIView):
   queryset = Medication.objects.all()
   serializer_class = MedicationSerializer
 
+class MedicationDetail(RetrieveAPIView):
+  queryset = Medication.objects.all()
+  serializer_class = MedicationSerializer
+  lookup_url_kwarg = 'medication_id'
+
+class MedicationUpdate(UpdateAPIView):
+  queryset = Medication.objects.all()
+  serializer_class = MedicationSerializer
+
+class MedicationDelete(DestroyAPIView):
+  queryset = Medication.objects.all()
+  serializer_class = MedicationSerializer
