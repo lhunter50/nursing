765a72c14145cf0dcf54ec2b881448693fba46b7
lhunter50 <luxstme@gmail.com>
2025-05-17 18:53:22 +0100

    change frequency to enum

--- base/models.py
+++ base/models.py
@@ -5,13 +5,24 @@ from django.urls import reverse
 
 
 class Medication(models.Model):
+    class Frequency(models.TextChoices):
+        ONCE_DAILY = 'QD', 'Once Daily'
+        TWICE_DAILY = 'BID', 'Twice Daily'
+        THREE_TIMES_DAILY = 'TID', 'Three Times Daily'
+        FOUR_TIMES_DAILY = 'QID', 'Four Times Daily'
+        AS_NEEDED = 'PRN', 'As Needed' 
+
     name = models.CharField(max_length = 200)
     classification = models.CharField(max_length=200)
     intention = models.CharField(max_length=200)
     implications = models.CharField(max_length=200)
     dose = models.CharField(max_length=200)
     route = models.CharField(max_length=200)
-    frequency = models.CharField(max_length=200)
+    frequency = models.CharField(
+        max_length = 3,
+        choices=Frequency.choices,
+        default=Frequency.ONCE_DAILY,
+    )
 
     class Meta:
         ordering = ['-name']
@@ -19,5 +30,3 @@ class Medication(models.Model):
     def __str__(self):
         return self.name
     
-    # def get_absolute_path(self):
-    #     return reverse('medication-detail', kwargs={"pk": self.pk})
